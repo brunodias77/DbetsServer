5994b24ec9cbbed7b6241abe06db2fdcc31f59ec
brunodias77 <brunohdias95@gmail.com>
2025-09-06 15:17:06 -0300

    ...

--- src/Dbets.Api/Controllers/AuthController.cs
+++ src/Dbets.Api/Controllers/AuthController.cs
@@ -1,6 +1,34 @@
+using Dbets.Application.Commands.Users.CreateUserCommand;
+using Dbets.Domain.Mediator;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Dbets.Api.Controllers;
 
-public class AuthController
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
 {
-    
+    public AuthController(IMediator mediator, ILogger<AuthController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    private readonly IMediator _mediator;
+    private readonly ILogger<AuthController> _logger;
+
+    [HttpPost("create-user")]
+    public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
+    {
+        try
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Erro durante o registro do usuário");
+            return BadRequest(new { message = "O registro falhou", error = ex.Message });
+        }
+    }
 }
